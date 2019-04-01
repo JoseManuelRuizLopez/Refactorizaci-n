113a6e0d2a49b3f1ee066d1f9d3cb089e2ac4efa
luisaoprog <luiszamora1995@gmail.com>
2019-04-01 12:17:09 +0200

    Primer commit

---
+++ Calcular Precio/src/es/studium/CalcularPrecio/CalcularPrecio.java
@@ -0,0 +1,12 @@
+package es.studium.CalcularPrecio;
+
+public class CalcularPrecio {
+
+	public double calcularPrecio(int unidades, double precioUnitario)
+	{
+		double precio = unidades * precioUnitario;
+		
+		return precio;
+	}
+
+}
