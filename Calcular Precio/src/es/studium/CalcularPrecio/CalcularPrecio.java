5348a8760a2b585bedf2784acc5a5c9fb1e78114
Jose Manuel Ruiz Lopez <JoseManuelRuizLopez97@gmail.com>
2019-04-01 12:34:12 +0200

    Segundo commit

--- Calcular Precio/src/es/studium/CalcularPrecio/CalcularPrecio.java
+++ Calcular Precio/src/es/studium/CalcularPrecio/CalcularPrecio.java
@@ -3,8 +3,11 @@ package es.studium.CalcularPrecio;
 public class CalcularPrecio {
 
 	public double calcularPrecio(int unidades, double precioUnitario)
-	{
-		double precio = unidades * precioUnitario;
+	{		
+		double iva = (precioUnitario * 21)/100;
+	
+		double precio = unidades * precioUnitario + iva;
+		
 		return precio;
 	}
 
